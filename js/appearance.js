e33fbd6d517dba2d5924be40bc01b49178fba833
Dolen Le <le2@cooper.edu>
2015-05-12 05:04:37 -0400

    added mirror style and gui

--- js/appearance.js
+++ js/appearance.js
@@ -2,34 +2,29 @@ visualStyles = {
 	flat: {
 		name: "FlatColors",
 		bgColor: 0x0F0F0F,
-		floorMaterial: new THREE.MeshPhongMaterial({
-			"color": 0x606060,
-			"emissive": 0,
-			"specular": 1118481,
-			"shininess": 30
+		floorMaterial: new THREE.MeshBasicMaterial({
+				"color": (Math.random()/7)*0xFFFFFF
 		}),
 		ballMaterial: new THREE.MeshPhongMaterial({
-			"color": 0x2F0000,
-			"emissive": 0x2F0000,
-			"specular": 0x009900,
+			"color": (Math.random())*0xFFFFFF,
+			"emissive": 0,
+			"specular": 0,
 			"shininess": 30
 		}),
-		ballRandomColor: false,
+		ballGlow: false,
 		blockMaterial: new THREE.MeshPhongMaterial({
 			"emissive": 0x090909,
 			"specular": 0x0F0F0F,
 			"shininess": 10
 		}),
-		blockRandomColor: true,
+		blockRefraction: false,
 		paddleMaterial: new THREE.MeshPhongMaterial({
 			"color": 0xFFFFFF
 		}),
-		ambientLight: new THREE.AmbientLight(0x1F1F1F),
+		ambientLight: new THREE.AmbientLight(0x8F8F8F),
 		pointLighting: [
 			new THREE.PointLight(0xffffff, 0.5),
 			new THREE.Vector3(0,400,0),
-			new THREE.PointLight(0xffffff, 1),
-			new THREE.Vector3(0,300,400)
 		]
 	},
 	
@@ -43,14 +38,14 @@ visualStyles = {
 			"shininess": 30
 		}),
 		ballMaterial: new THREE.MeshPhongMaterial({
-			"color": 0x2F0000,
+			"color": 0x001C6F,
 			"emissive": 0x2F0000,
 			"specular": 0x009900,
 			"shininess": 30,
 			"transparent": true,
 			"opacity": 0.7
 		}),
-		ballRandomColor: false,
+		ballGlow: true,
 		blockMaterial: new THREE.MeshPhongMaterial({
 			"emissive": 0x090909,
 			"specular": 0x0F0F0F,
@@ -58,11 +53,11 @@ visualStyles = {
 			"transparent": true,
 			"opacity": 0.5
 		}),
-		blockRandomColor: true,
+		blockRefraction: false,
 		paddleMaterial: new THREE.MeshPhongMaterial({
 			"color": 0xFFFFFF,
 			"transparent": true,
-			"opacity": 0.7
+			"opacity": 0.8
 		}),
 		ambientLight: new THREE.AmbientLight(0x1F1F1F),
 		pointLighting: [
@@ -71,5 +66,39 @@ visualStyles = {
 			new THREE.PointLight(0xffffff, 0.5),
 			new THREE.Vector3(-200,300,-200)
 		]
+	},
+	
+	mirror: {
+		name: "Mirror",
+		bgColor: 0x0A0A0A,
+		floorMaterial: new THREE.MeshPhongMaterial({
+			"color": 0x181818,
+			"specular": 0x111111,
+			"shininess": 30,
+		}),
+		floorReflection: true,
+		ballMaterial: new THREE.MeshPhongMaterial({
+			"color": 0xFFFFFF,
+			"specular": 0x002908
+		}),
+		ballGlow: false,
+		blockMaterial: new THREE.MeshPhongMaterial({
+			"specular": 0x0F0F0F,
+			"emissive": 0x0A0A0A,
+			"shininess": 50,
+		}),
+		blockRefraction: true,
+		paddleMaterial: new THREE.MeshPhongMaterial({
+			"color": 0x1B1B1B,
+			"transparent": true,
+			"opacity": 0.85
+		}),
+		ambientLight: new THREE.AmbientLight(0x1F1F1F),
+		pointLighting: [
+			new THREE.PointLight(0xffffff, 1),
+			new THREE.Vector3(0,400,0),
+			new THREE.HemisphereLight(0xffffff, 0.3),
+			new THREE.Vector3(-200,300,-200)
+		]
 	}
-};
+}; 
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 var scene, camera, renderer;
 
-var time, prevTime, controls;
+var time, prevTime, controls, cubeURL, cubeMapTexture;
 var block, paddle, ball, hitbox;
 var balls = [];
 
 var gameLength = 400;
-var gameWidth = 300;
+var gameWidth = 250;
 var gameHeight = 100;
 
 var blockWidth = 25;
@@ -18,9 +18,12 @@ var layerSpacing = 3;
 var maxCols = Math.floor(gameWidth/(blockWidth+colSpacing));
 var maxLayers = Math.floor(gameHeight/(blockHeight+layerSpacing));
 
+var ballColor;
+var ballGlow = false;
 var ballRadius = 5;
 var paddleWidth = 50;
 var paddleDepth = 5;
+var wallDepth = 100;
 
 var blockGeometry;
 var paddleGeometry;
@@ -34,6 +37,11 @@ var lightingGroup = new THREE.Group();
 
 var raycaster = new THREE.Raycaster();
 var group = new THREE.Group();
+var wallGroup = new THREE.Group();
+var floorMirror;
+var ballCamera;
+
+var score = 0;
 
 init();
 setTimeout( ballRadius, 5000 );
@@ -41,40 +49,89 @@ animate();
 
 function init() {
 	//Stuff
-	//paddleGeometry = new THREE.CylinderGeometry(5,20, gameHeight, 10 );
-	paddleGeometry = new THREE.BoxGeometry(paddleWidth, gameHeight, paddleDepth*2);	
+	paddleGeometry = new THREE.Geometry();
+	paddleGeometry.vertices.push(
+		new THREE.Vector3(-paddleWidth/2,  gameHeight/2, paddleDepth),
+		new THREE.Vector3(-paddleWidth/2+10,  gameHeight/2, -paddleDepth),
+		new THREE.Vector3(paddleWidth/2,  gameHeight/2, paddleDepth),
+		new THREE.Vector3(paddleWidth/2-10,  gameHeight/2, -paddleDepth),
+		
+		new THREE.Vector3(-paddleWidth/2,  -gameHeight/2, paddleDepth),
+		new THREE.Vector3(-paddleWidth/2+10,  -gameHeight/2, -paddleDepth),
+		new THREE.Vector3(paddleWidth/2,  -gameHeight/2, paddleDepth),
+		new THREE.Vector3(paddleWidth/2-10,  -gameHeight/2, -paddleDepth)
+	);
+
+	paddleGeometry.faces.push(
+		new THREE.Face3(0,3,1),
+		new THREE.Face3(0,2,3),
+		new THREE.Face3(0,6,2),
+		new THREE.Face3(0,4,6),
+		new THREE.Face3(0,5,4),
+		new THREE.Face3(0,1,5),
+		new THREE.Face3(1,3,7),
+		new THREE.Face3(1,7,5),
+		new THREE.Face3(3,6,7),
+		new THREE.Face3(3,2,6)
+	);
+	paddleGeometry.computeFaceNormals();
+	//paddleGeometry = new THREE.CylinderGeometry(paddleDepth*2,paddleDepth, gameHeight, 10 );
+	//paddleGeometry = new THREE.BoxGeometry(paddleWidth, gameHeight, paddleDepth*2);	
 	
 	scene = new THREE.Scene();
 	renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	//renderer.setPixelRatio(window.devicePixelRatio);
+	//renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(Math.min(window.innerWidth, 1280), Math.min(window.innerHeight, 800));
+	$('#score').outerWidth(Math.min(window.innerWidth, 1280));
+	$('#scorecounter').text(score);
 	
 	//Set up camera perspective projection
-	var aspect = window.innerWidth/window.innerHeight; 
+	//var aspect = window.innerWidth/window.innerHeight; 
+	var aspect = Math.min(window.innerWidth, 1280)/Math.min(window.innerHeight, 800);
 	camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 100000);
 	camera.position.set(0,400,400);
 	camera.lookAt(scene.position);
 	
+	//skybox
+	var urlPrefix = "cubemap/inside1/";
+	cubeURL = [ urlPrefix + "0004.png", urlPrefix + "0002.png",
+		urlPrefix + "0006.png", urlPrefix + "0005.png",
+		urlPrefix + "0001.png", urlPrefix + "0003.png" ];
+	cubeMapTexture = THREE.ImageUtils.loadTextureCube(cubeURL, THREE.CubeRefractionMapping);
+	var cubeMapShader = THREE.ShaderLib["cube"];
+	cubeMapShader.uniforms[ "tCube" ].value = cubeMapTexture;
+	var skyboxMaterial = new THREE.ShaderMaterial({
+		fragmentShader: cubeMapShader.fragmentShader,
+		vertexShader: cubeMapShader.vertexShader,
+		uniforms: cubeMapShader.uniforms,
+		side: THREE.BackSide
+	});
+	scene.add(new THREE.Mesh(new THREE.BoxGeometry(8000, 8000, 8000), skyboxMaterial));
+	
 	loadAppearance(visualStyles.flat);
 	scene.add(lightingGroup);
 	
-	var urlPrefix = "cubemap/inside1/";
-	urls = [ urlPrefix + "0004.png", urlPrefix + "0002.png",
-		urlPrefix + "0006.png", urlPrefix + "0005.png",
-		urlPrefix + "0001.png", urlPrefix + "0003.png" ];
-	
-	var materialArray = [];
-	for (var i = 0; i < 6; i++)
-		materialArray.push( new THREE.MeshBasicMaterial({
-			map: THREE.ImageUtils.loadTexture( urls[i] ),
-			side: THREE.BackSide
-		}));
-	scene.add(new THREE.Mesh(new THREE.BoxGeometry(8000, 8000, 8000), new THREE.MeshFaceMaterial(materialArray)));
-	
 	//Add floor
 	floor = new THREE.Mesh(new THREE.PlaneGeometry(gameWidth+ballRadius,gameLength+ballRadius), floorMaterial);
-	floor.rotateOnAxis(new THREE.Vector3(1,0,0), -1.57); //rotate 90deg to face horizontally
+	floor.rotateOnAxis(new THREE.Vector3(1,0,0), -Math.PI/2); //rotate 90deg to face horizontally
+	if(floorMirror)
+		floor.add(floorMirror);
 	scene.add(floor);
+	
+	//Add walls
+	var wallMaterial = new THREE.MeshPhongMaterial({"color":0x8F8F8F});
+	var leftWall = new THREE.Mesh(new THREE.BoxGeometry(wallDepth, gameHeight, gameLength+ballRadius), wallMaterial);
+	var rightWall = new THREE.Mesh(new THREE.BoxGeometry(wallDepth, gameHeight, gameLength+ballRadius), wallMaterial);
+	var rearWall = new THREE.Mesh(new THREE.BoxGeometry(gameWidth+2*wallDepth+ballRadius, gameHeight, wallDepth), wallMaterial);
+	rightWall.position.x = gameWidth/2+wallDepth/2+ballRadius/2;
+	leftWall.position.x = -rightWall.position.x;
+	rearWall.position.z = -gameLength/2-wallDepth/2-ballRadius/2;
+	rearWall.position.y = leftWall.position.y = rightWall.position.y = gameHeight/2;
+	wallGroup.add(leftWall);
+	wallGroup.add(rightWall);
+	wallGroup.add(rearWall);
+	//scene.add(wallGroup);
 	
 	//Set up block
 	block = new THREE.Mesh(new THREE.BoxGeometry(blockWidth,blockHeight,blockLength), blockMaterial);
@@ -82,34 +139,19 @@ function init() {
 	
 	//Set up paddle
 	paddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
-	paddle.position.y = paddleGeometry.parameters.height/2;
+	paddle.position.y = gameHeight/2;
 	paddle.position.z = gameLength/2-20;
 	group.add(paddle);
 	scene.add(group)
 	
 	//Set up ball
-	//Test shader
-	ballShader = new THREE.ShaderMaterial({
-	    uniforms: 
-		{ 
-			"c":   { type: "f", value: 1.0 },
-			"p":   { type: "f", value: 0.3 },
-			glowColor: { type: "c", value: new THREE.Color(0x0022FF) },
-			viewVector: { type: "v3", value: camera.position }
-		},
-		vertexShader: $("#vertexShader").text(),
-		fragmentShader: $("#fragmentShader").text(),
-		side: THREE.FrontSide,
-		blending: THREE.AdditiveBlending,
-		transparent: true
-	});
 	addBall();
 
-	//Initalize Controls
+	//Initialize Controls
 	controls = new THREE.OrbitControls(camera, renderer.domElement);
 	controls.minPolarAngle = 0;
 	controls.maxPolarAngle = Math.PI/2;
-	document.body.appendChild(renderer.domElement);
+	$('#game').append(renderer.domElement);
 	
 	document.addEventListener('mousemove', function(event) {
 		paddle.position.x = (event.clientX/window.innerWidth) * gameWidth - gameWidth/2;
@@ -117,15 +159,25 @@ function init() {
 			paddle.loadedBall.position.x = paddle.position.x;
 	});
 	
-	$("#addBall").click(function () {
+	$("#addBall").click(function() {
 		addBall();
 		$("#addBall").blur();
+	});
+	
+	$("#wallToggle").change(function() {
+		if($("#wallToggle").prop("checked")) {
+			scene.add(wallGroup);
+			console.log("checked")
+		} else {
+			scene.remove(wallGroup);
+		}
+		$("#wallToggle").blur();
 	});
 	
 	$(window).keypress(function(event) {
 		if((event.keyCode == 32 || event.keyCode == 0) && paddle.loadedBall) {
 			event.preventDefault();
-			paddle.loadedBall.direction.set(Math.random()-0.5, 0, -0.5).normalize();
+			paddle.loadedBall.direction.set(Math.random()-0.5, Math.random()-0.5, -0.6).normalize();
 			balls.push(paddle.loadedBall);
 			paddle.loadedBall = false;
 		}
@@ -136,44 +188,63 @@ function init() {
 
 function animate() {
 	time = performance.now();
+	if(floorMirror) {
+		floorMirror.render();
+		if(balls.length>0) {
+			ballCamera.position.copy(balls[0].position);
+		} else if(paddle.loadedBall) {
+			ballCamera.position.copy(paddle.loadedBall.position);
+		}
+		ballCamera.updateCubeMap(renderer, scene);
+	}
 	renderer.render(scene, camera);
 	controls.update();
 	update(time-prevTime);
 	prevTime = time;
-	//if(!paddle.loadedBall)
-	//	paddle.position.x = balls[0].position.x;
-
+	// if(!paddle.loadedBall)
+		// paddle.position.x = balls[0].position.x;
 	requestAnimationFrame(animate);
 }
 
 function update(delta) {
 	//delta = clock.getDelta();
-	for(var b=0; b < balls.length; b++) {
+	for(var b=0; b < balls.length; b++ ) {
 		ball = balls[b];
-		if (ball.position.x < -gameWidth/2 || ball.position.x > gameWidth/2) {
+		if(ball.position.x < -gameWidth/2) {
 			ball.direction.x = -ball.direction.x;
 			ball.justBounced = false;
+			ball.position.x = -gameWidth/2;
+		}
+		if(ball.position.x > gameWidth/2) {
+			ball.direction.x = -ball.direction.x;
+			ball.justBounced = false;
+			ball.position.x = gameWidth/2;
 		}
 		if(ball.position.z < -gameLength/2) {
 			ball.direction.z = -ball.direction.z;
 			ball.justBounced = false;
+			ball.position.z = -gameLength/2;
 		}
 		if(ball.position.z > gameLength/2) {
 			balls.splice(b, 1);
 			group.remove(ball);
-			console.log("NO WRONG");
+			addBall();
 			continue;
+			// ball.direction.z = -ball.direction.z;
+			// ball.justBounced = false;
+			// ball.position.z = gameLength/2;
 		}
-		if(ball.position.y < ballRadius || ball.position.y > gameHeight) {
+		if(ball.position.y < ballRadius) {
 			ball.direction.y = -ball.direction.y;
 			ball.justBounced = false;
+			ball.position.y = ballRadius;
 		}
-		
-		// Prevent balls from getting stuck on edges
-		ball.position.x = Math.max(-gameWidth/2, Math.min(gameWidth/2, ball.position.x));
-		ball.position.z = Math.max(-gameLength/2, Math.min(gameLength/2, ball.position.z));
-		ball.position.y = Math.max(ballRadius, Math.min(gameHeight, ball.position.y));
-		
+		if(ball.position.y > gameHeight) {
+			ball.direction.y = -ball.direction.y;
+			ball.justBounced = false;
+			ball.position.y = gameHeight;
+		}
+				
 		hitbox = ball.children[0];
 	
 		for(var i = 0; i < hitbox.geometry.vertices.length; i++) {
@@ -181,33 +252,63 @@ function update(delta) {
 			raycaster.set(ball.position, directionVector.clone().normalize() );
 			
 			var collisionResults = raycaster.intersectObjects(group.children);
-			if( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) {
+			if(collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()) {
 				var obj = collisionResults[0].object;
 				if(obj.isBlock) {
 					group.remove(obj);
 					ball.direction.reflect(collisionResults[0].face.normal);
 					ball.justBounced = false;
-					//break;
-				} 
-				else if(!ball.justBounced) { //prevent balls from getting stuck inside the paddle
-					//ball.position.add(collisionResults[0].face.normal.clone().multiplyScalar(ball.speed.length()*2));
+					$('#scorecounter').text(++score);
+				} else if(!ball.justBounced) { //prevent balls from getting stuck inside the paddle
 					ball.direction.reflect(collisionResults[0].face.normal);
 					ball.justBounced = true;
 					break;
 				}
-				//break;
-			}
+			} 
+			// else if(Math.abs(ball.position.x - paddle.position.x) < paddleWidth+ballRadius && Math.abs(ball.position.z - paddle.position.z) < ballRadius+paddleDepth) {
+				// ball.direction.reflect(new THREE.Vector3(0,0,1));
+				// ball.justBounced = true;
+				// break;
+			// }
 		}
-		
-		ball.position.add(ball.speed.copy(ball.direction).multiplyScalar(delta/9));
+		ball.position.add(ball.speed.copy(ball.direction).multiplyScalar(delta/5));
 	}
 
 }
 
 function loadAppearance(style) {
-	floorMaterial = style.floorMaterial;
+	if(style.ballGlow) {
+		ballShader = new THREE.ShaderMaterial({
+			uniforms: 
+			{
+				"c":   {type: "f", value: 0.9},
+				"p":   {type: "f", value: 0.7},
+				glowColor: {type: "c", value: new THREE.Color(style.ballMaterial.color)},
+				viewVector: {type: "v3", value: camera.position}
+			},
+			vertexShader: $("#vertexShader").text(),
+			fragmentShader: $("#fragmentShader").text(),
+			side: THREE.FrontSide,
+			blending: THREE.AdditiveBlending,
+			transparent: true
+		});
+		ballGlow = new THREE.Mesh(new THREE.SphereGeometry(ballRadius*1.5, 16), ballShader);
+	}
 	ballMaterial = style.ballMaterial;
+	if(style.floorReflection == true) {
+		floorMirror = new THREE.Mirror(renderer, camera, { clipBias: 0.03, textureWidth: gameWidth*2, textureHeight: gameLength*2, color: 0x707070 } );
+		floorMaterial = floorMirror.material;
+		ballCamera = new THREE.CubeCamera(ballRadius, 10000, ballRadius);
+		scene.add(ballCamera);
+		ballMaterial.envMap = ballCamera.renderTarget;
+	} else {
+		floorMaterial = style.floorMaterial;
+	}
 	blockMaterial = style.blockMaterial;
+	if(style.blockRefraction == true) {
+		blockMaterial.envMap=cubeMapTexture;
+		blockMaterial.refractionRatio=0.8;
+	}
 	paddleMaterial = style.paddleMaterial;
 	renderer.setClearColor(style.bgColor);
 	if(style.ambientLight) {
@@ -221,15 +322,18 @@ function loadAppearance(style) {
 }
 
 function blockWall(rows) {
+	var counter = 0;
 	for(var z = 0; z < rows; z++) {
 		var material = blockMaterial.clone();
 		material.color.setHex(Math.random() * 0xffffff);
 		for(var x = 0; x < maxCols; x++) {
 			for(var y = 0; y < maxLayers; y++) {
 				blockAt(x,y,z,material);
+				counter++;
 			}
 		}
-	} 
+	}
+	console.log(counter+" blocks added");
 }
 
 function blockAt(x, y, z, blockMaterial) {
@@ -253,14 +357,14 @@ function addBall() {
 		ball.justBounced = false;
 		
 		//x = diameter/sqrt(3) THREE.DodecahedronGeometry(ballRadius));
-		var hitbox = new THREE.Mesh(new THREE.BoxGeometry(ballRadius*2/1.7,ballRadius*2/1.7,ballRadius*2/1.7));
+		var hitbox = new THREE.Mesh(new THREE.DodecahedronGeometry(ballRadius));
 		hitbox.visible = false;
 		ball.add(hitbox);
 		
-		var glow = new THREE.Mesh(new THREE.SphereGeometry(ballRadius*1.2, 16), ballShader);
-		ball.add(glow);
-		
-		ball.add(new THREE.PointLight(0x001188));
+		if(ballGlow) {
+			ball.add(ballGlow.clone());
+			ball.add(new THREE.PointLight(ball.material.color));
+		}
 		
 		ball.speed = new THREE.Vector3();
 		ball.direction = new THREE.Vector3();//.set(Math.random()-0.5, 0, -0.5).normalize();
